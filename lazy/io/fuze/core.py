01c372419f9678890d957774b8ffe729b97ecbe6
trisongz <tri@songzcorp.com>
2022-01-05 15:13:46 -0600

    ugly

--- lazy/io/fuze/core.py
+++ lazy/io/fuze/core.py
@@ -13,7 +13,7 @@ import pathlib
 from importlib import import_module
 from fsspec import AbstractFileSystem
 from fsspec.asyn import AsyncFileSystem
-from fsspec.fuse import run as run_fuze
+from fsspec.fuse import FUSEr, FUSE
 from lazy.libz import Lib
 from typing import List, Dict, Type, Tuple, Any, Optional, Union
 from types import ModuleType
@@ -21,11 +21,13 @@ from types import ModuleType
 from logz import get_logger
 #try: from multiprocess import Process
 #except ImportError: 
+import threading
 import multiprocessing as mp
 #from multiprocessing import Process
 
 ## Keep track of all mounted
-_ALL_FUZES: Dict[str, Type[mp.Process]] = {}
+#_ALL_FUZES: Dict[str, Type[mp.Process]] = {}
+_ALL_FUZES: Dict[str, threading.Thread] = {}
 
 _FUZE_READY: bool = False
 _FUZE_ALLOWED: bool = not sys.platform.startswith('win')
@@ -43,7 +45,7 @@ def _prepare_fuze():
     Lib.import_lib('multiprocess')
     _FUZE_READY = True
 
-def _add_proc(path: str, proc: Type[mp.Process]):
+def _add_proc(path: str, proc: threading.Thread):
     global _ALL_FUZES
     _ALL_FUZES[path] = proc
 
@@ -51,8 +53,73 @@ def _kill_proc(path: str, timeout: int = 5):
     global _ALL_FUZES
     if _ALL_FUZES.get(path):
         proc = _ALL_FUZES.pop(path)
-        os.kill(proc.pid, signal.SIGKILL)
+        #proc.kill()
+        #os.kill(proc.pid, signal.SIGKILL)
         proc.join(timeout=timeout)
+
+def _run_fuze(fs,
+    path,
+    mount_point,
+    foreground=True,
+    threads=False,
+    ready_file=False,
+    ops_class=FUSEr,
+):
+    """Mount stuff in a local directory
+    This uses fusepy to make it appear as if a given path on an fsspec
+    instance is in fact resident within the local file-system.
+    This requires that fusepy by installed, and that FUSE be available on
+    the system (typically requiring a package to be installed with
+    apt, yum, brew, etc.).
+    Parameters
+    ----------
+    fs: file-system instance
+        From one of the compatible implementations
+    path: str
+        Location on that file-system to regard as the root directory to
+        mount. Note that you typically should include the terminating "/"
+        character.
+    mount_point: str
+        An empty directory on the local file-system where the contents of
+        the remote path will appear.
+    foreground: bool
+        Whether or not calling this function will block. Operation will
+        typically be more stable if True.
+    threads: bool
+        Whether or not to create threads when responding to file operations
+        within the mounter directory. Operation will typically be more
+        stable if False.
+    ready_file: bool
+        Whether the FUSE process is ready. The `.fuse_ready` file will
+        exist in the `mount_point` directory if True. Debugging purpose.
+    ops_class: FUSEr or Subclass of FUSEr
+        To override the default behavior of FUSEr. For Example, logging
+        to file.
+    """
+    p = pathlib.Path(mount_point).resolve(True)
+    p.mkdir(parents=True, exist_ok=True)
+    func = lambda: FUSE(
+        ops_class(fs, path, ready_file=ready_file),
+        p.as_posix(),
+        nothreads=not threads,
+        foreground=foreground,
+    )
+    
+    if not foreground:
+        th = threading.Thread(target=func)
+        th.daemon = True
+        th.start()
+        logger.info(f'Started Mount {path} -> {p.as_posix()} in Background')
+        _add_proc(mount_point, th)
+        return th
+    else:  # pragma: no cover
+        try:
+            logger.info(f'Started Mount {path} -> {p.as_posix()} in Foreground')
+            func()
+        except KeyboardInterrupt:
+            pass
+
+
 
 _PROCESS_SET: bool = False
 
@@ -117,23 +184,25 @@ class BaseFuzerCls:
     
 
     @classmethod
-    def _run_fuze(cls, source: str, mount_path: str, ready_file: bool = True, foreground: bool = False, threads: bool = True, fs_args: Dict[str, Any] = {}, *args, **kwargs):
+    def __run_fuze(cls, source: str, mount_path: str, ready_file: bool = True, foreground: bool = False, threads: bool = True, fs_args: Dict[str, Any] = {}, *args, **kwargs):
         p = pathlib.Path(mount_path).resolve(True)
         p.mkdir(parents=True, exist_ok=True)
         fs = cls.get_filesystem(fs_args, *args, **kwargs)
-        run_fuze(fs, source, p.as_posix(), ready_file=ready_file, foreground=foreground, threads=threads)
+        #run_fuze(fs, source, p.as_posix(), ready_file=ready_file, foreground=foreground, threads=threads)
 
 
     @classmethod
-    def mount(cls, source: str, mount_path: str, ready_file: bool = True, foreground: bool = False, threads: bool = False, fs_args: Dict[str, Any] = {}, daemon: bool = False, *args, **kwargs) -> None:
+    def mount(cls, source: str, mount_path: str, ready_file: bool = True, foreground: bool = False, threads: bool = False, fs_args: Dict[str, Any] = {}, *args, **kwargs) -> None:
         assert mount_path not in _ALL_FUZES, f'{mount_path} is already mounted from {source}'
         _prepare_fuze()
         #from multiprocess import Process
-        logger.info('starting mount')
-        mount_proc = _get_process()(target=cls._run_fuze, args=(source, mount_path, ready_file, foreground, threads, fs_args, *args), kwargs=kwargs, daemon=daemon)
-        mount_proc.start()
-        logger.info(f'started proc: {mount_proc.pid}')
-        _add_proc(mount_path, mount_proc)
+        logger.info('Starting mount')
+        fs = cls.get_filesystem(fs_args, *args, **kwargs)
+        _run_fuze(fs, source, mount_path, foreground=foreground, threads=threads, ready_file=ready_file)
+        #mount_proc = _get_process()(target=cls._run_fuze, args=(source, mount_path, ready_file, foreground, threads, fs_args, *args), kwargs=kwargs, daemon=daemon)
+        #mount_proc.start()
+        #logger.info(f'started proc: {mount_proc.pid}')
+        #_add_proc(mount_path, mount_proc)
     
     @classmethod
     def unmount(cls, mount_path: str) -> None:
