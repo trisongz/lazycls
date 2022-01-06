c4b2d233a9a0b331e85cfd69b78ee1779dc60f49
trisongz <tri@songzcorp.com>
2022-01-05 23:20:41 -0600

    Updating

--- lazy/configz/cloudz.py
+++ lazy/configz/cloudz.py
@@ -59,25 +59,49 @@ class CloudAuthz(ConfigCls):
     s3compat_access_token: Optional[str] = ""
     s3compat_config: Optional[pyd.Json] = None
 
+    class Config:
+        auto_init = True
+
     @classmethod
     def get_s3_endpoint(cls):
-        _authz = cls()
-        return f'https://s3.{_authz.aws_region}.amazonaws.com'
+        #_authz = cls()
+        return f'https://s3.{cls.aws_region}.amazonaws.com'
 
     @classmethod
     def get_boto_path(cls):
-        _authz = cls()
-        if _authz.boto_config and _authz.boto_config.exists(): return _authz.boto_config
-        _authz.authz_dir.mkdir(parents=True, exist=True)
-        return _authz.authz_dir.joinpath('.boto')
+        if cls.boto_config and cls.boto_config.exists(): return cls.boto_config
+        cls.authz_dir.mkdir(parents=True, exist=True)
+        return cls.authz_dir.cast().joinpath('.boto')
+        #_authz = cls()
+        #if _authz.boto_config and _authz.boto_config.exists(): return _authz.boto_config
+        #_authz.authz_dir.mkdir(parents=True, exist=True)
+        #return _authz.authz_dir.cast().joinpath('.boto')
     
     @classmethod
     def should_write_boto(cls):
-        _authz = cls()
-        return not bool(_authz.boto_config and _authz.boto_config.exists())
+        return not bool(cls.boto_config and cls.boto_config.exists())
+        #_authz = cls()
+        #return not bool(_authz.boto_config and _authz.boto_config.exists())
 
     @classmethod
     def get_boto_values(cls):
+        t = "[Credentials]\n"
+        if cls.aws_access_key_id:
+            t += f"aws_access_key_id = {cls.aws_access_key_id}\n"
+            t += f"aws_secret_access_key = {cls.aws_secret_access_key}\n"
+        if cls.gauth.to_env_path.exists():
+            t += f"gs_service_key_file = {cls.gauth}\n"
+        t += "\n[Boto]\n"
+        t += "https_validate_certificates = True\n"
+        t += "\n[GSUtil]\n"
+        t += "content_language = en\n"
+        t += "default_api_version = 2\n"
+        if cls.gcloud_project or cls.google_cloud_project:
+            t += f"default_project_id = {cls.gcloud_project or cls.google_cloud_project}\n"
+        return t
+
+    @classmethod
+    def _get_boto_values(cls):
         _authz = cls()
         t = "[Credentials]\n"
         if _authz.aws_access_key_id:
@@ -96,43 +120,43 @@ class CloudAuthz(ConfigCls):
 
     @classmethod
     def write_botofile(cls, overwrite: bool = False, **kwargs):
-        _authz = cls()
-        if _authz.should_write_boto:
-            p = _authz.get_boto_path()
+        #_authz = cls()
+        if cls.should_write_boto:
+            p = cls.get_boto_path()
             if not p.exists() or overwrite:
                 logger.info(f'Writing Botofile to {p.as_posix()}')
-                p.write_text(_authz.get_boto_values())
+                p.write_text(cls.get_boto_values())
             else: 
                 logger.error(f'Botofile {p.as_posix()} exists and overwrite is False. Not overwriting')
             return p
         else: 
-            logger.warning(f'Skipping writing Botofile as BotoConfig = {_authz.boto_config.as_posix()} exists')
+            logger.warning(f'Skipping writing Botofile as BotoConfig = {cls.boto_config.as_posix()} exists')
             return cls.boto_config
 
 
     @classmethod
     def set_authz_env(cls):
         from lazy.cmd.contrib import export
-        _authz = cls()
-        if _authz.gcp_auth: export(GOOGLE_APPLICATION_CREDENTIALS=_authz.gcp_auth)
-        elif _authz.gauth: export(GOOGLE_APPLICATION_CREDENTIALS=_authz.gauth)
+        #_authz = cls()
+        if cls.gcp_auth: export(GOOGLE_APPLICATION_CREDENTIALS=cls.gcp_auth)
+        elif cls.gauth: export(GOOGLE_APPLICATION_CREDENTIALS=cls.gauth)
 
-        if _authz.gcloud_project: export(GOOGLE_CLOUD_PROJECT=_authz.gcloud_project or _authz.google_cloud_project)
-        botopath = _authz.get_boto_path()
+        if cls.gcloud_project: export(GOOGLE_CLOUD_PROJECT=cls.gcloud_project or cls.google_cloud_project)
+        botopath = cls.get_boto_path()
         ## We know this is our custom botofile
-        if botopath.exists() and _authz.should_write_boto():
+        if botopath.exists() and cls.should_write_boto():
             export(BOTO_PATH=botopath.as_posix())
             export(BOTO_CONFIG=botopath.as_posix())
-        if _authz.aws_access_key_id:
-            export(AWS_ACCESS_KEY_ID=_authz.aws_access_key_id)
-            export(AWS_SECRET_ACCESS_KEY=_authz.aws_secret_access_key)
-        if _authz.set_s3_endpoint:
-            export(S3_ENDPOINT=_authz.get_s3_endpoint())
-        if _authz.minio_access_key:
-            export(MINIO_ACCESS_KEY=_authz.minio_access_key)
-            export(MINIO_SECRET_KEY=_authz.minio_secret_key)
-        if _authz.minio_endpoint:
-            export(MINIO_ENDPOINT=_authz.minio_endpoint)
+        if cls.aws_access_key_id:
+            export(AWS_ACCESS_KEY_ID=cls.aws_access_key_id)
+            export(AWS_SECRET_ACCESS_KEY=cls.aws_secret_access_key)
+        if cls.set_s3_endpoint:
+            export(S3_ENDPOINT=cls.get_s3_endpoint())
+        if cls.minio_access_key:
+            export(MINIO_ACCESS_KEY=cls.minio_access_key)
+            export(MINIO_SECRET_KEY=cls.minio_secret_key)
+        if cls.minio_endpoint:
+            export(MINIO_ENDPOINT=cls.minio_endpoint)
     
     @classmethod
     def update_authz(cls, **config):
