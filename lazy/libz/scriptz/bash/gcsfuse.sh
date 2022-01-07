ceaef2abfc635983b58828d65e17113d566a0c80
trisongz <tri@songzcorp.com>
2022-01-07 16:31:06 -0600

    Update gcsfuse.sh

--- lazy/libz/scriptz/bash/gcsfuse.sh
+++ lazy/libz/scriptz/bash/gcsfuse.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+BUCKET=$1
+MOUNT_PATH=$2
+
 SCRIPTS_DIR=$(dirname "$(realpath $0)")
 FUSE_FUNCS=${FUSE_FUNCS:-"$SCRIPTS_DIR/fuse_v1.sh"}
 
 source "$FUSE_FUNCS"
-gcsfuse_mount $1 $2
+
+gcsfuse_mount $BUCKET $MOUNT_PATH
